e4a419270920eaf5eecd68eaed480f29415146be
vec <vec@torzo.club>
2017-07-17 11:38:25 +0800

    admin gulpfile & gulp dest directories addition

--- gulpfile.js
+++ gulpfile.js
@@ -16,31 +16,61 @@ gulp.task('front-less', () => {
 			errorHandler: function (err) {
 				beep('*-*-')
 				notify.onError('Error: <%= error.message %>').apply(this, arguments)
-			}}
-		))
+			}
+		}))
 		.pipe(less({
 			paths: [ 'static/less/' ],
 		}))
 		.pipe(gulp.dest('static/style/'))
 })
-gulp.task('front_es6toes5', () => {
+gulp.task('front-es6toes5', () => {
 	return gulp.src("static/src/*.js")
 		.pipe(plumber({
 			errorHandler: function (err) {
-				beep('*-*-');
-				notify.onError('Error: <%= error.message %>').apply(this, arguments);
-			}}
-		))
+				beep('*-*-')
+				notify.onError('Error: <%= error.message %>').apply(this, arguments)
+			}
+		}))
 		.pipe(sourcemaps.init())
 		.pipe(babel())
 		.pipe(sourcemaps.write("."))
-		.pipe(gulp.dest("static/script/"));
+		.pipe(gulp.dest("static/script/"))
+})
+
+gulp.task('admin-front-less', () => {
+	return gulp.src('static/admin/less/*.less')
+		.pipe(plumber({
+			errorHandler(err) {
+				beep('*-*-')
+				notify.onError('Error: <%= error.message %>').apply(this, arguments)
+			}
+		}))
+		.pipe(less({
+			paths: ['static/admin/less']
+		}))
+		.pipe(gulp.dest('static/admin/style/'))
+})
+gulp.task('admin-front-es6toes5', () => {
+	return gulp.src('static/admin/src/*.js')
+		.pipe(plumber({
+			errorHandler: function (err) {
+				beep('*-*-')
+				notify.onError('Error: <%= error.message %>').apply(this, arguments)
+			}
+		}))
+		.pipe(sourcemaps.init())
+		.pipe(babel())
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('static/admin/script/'))
 })
 
 
 gulp.task('watch', () => {
-	gulp.watch('static/src/*.js', ['front_es6toes5']);
+	gulp.watch('static/src/*.js', ['front-es6toes5'])
 	gulp.watch('static/less/*.less', ['front-less'])
+
+	gulp.watch('static/admin/src/*.js', ['admin-front-es6toes5'])
+	gulp.watch('static/admin/less/*.less', ['admin-front-less'])
 })
 
 gulp.task('default', ['front-less', 'watch'])
